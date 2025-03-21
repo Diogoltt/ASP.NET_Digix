00268465941c34e180f821b10c5da44f9377f7c5
Diogoltt <diogoleite09@gmail.com>
2025-03-21 16:54:46 -0400

    feat: Aula 1

---
+++ Aula_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Aula_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Builder = WebApplication.CreateBuilder(args);
+            var app = Builder.Build();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.Redirect("/index.html");
+                });
+            });
+
+            app.Run();
+        }
+    }
+}
